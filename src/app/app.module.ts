3dcc6393ecf2765d9e805c6e71074c35aba61269
jason-ibisi <daveyjones@grr.la>
2018-10-16 11:26:18 +0100

    Added FontAwesome for AngularJS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,11 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
@@ -11,6 +16,9 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+
+// Add an icon to the library for convenient access in other components
+library.add(fas, far, fab);
 
 @NgModule({
   declarations: [
@@ -23,7 +31,8 @@ import { DishService } from './services/dish.service';
     BrowserAnimationsModule,
     MatToolbarModule,
     FlexLayoutModule,
-    MaterialModule
+    MaterialModule,
+    FontAwesomeModule
   ],
   providers: [
     DishService
