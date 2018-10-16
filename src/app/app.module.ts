736ffd335e4614a77ce6ad340d3376e7bffed51d
jason-ibisi <daveyjones@grr.la>
2018-10-16 10:35:22 +0100

    Angular Services Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
+import { DishService } from './services/dish.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -23,7 +25,9 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     FlexLayoutModule,
     MaterialModule
   ],
-  providers: [],
+  providers: [
+    DishService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,9 +1,11 @@
 import { Comment } from './comment';
 
 export class Dish {
+  id: string;
   name: string;
   image: string;
   category: string;
+  featured: boolean;
   label: string;
   price: string;
   description: string;
