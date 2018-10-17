fc3bbb84512cd124ffc2ee3d337ee4b11607b032
jason-ibisi <daveyjones@grr.la>
2018-10-17 11:58:12 +0100

    Single Page Application Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -48,7 +49,8 @@ library.add(fas, far, fab);
     AppRoutingModule
   ],
   providers: [
-    DishService
+    DishService,
+    PromotionService
   ],
   bootstrap: [AppComponent]
 })
