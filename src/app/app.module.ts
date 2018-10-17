c36d27e5009b8f77596594839b33fc40b521e104
jason-ibisi <daveyjones@grr.la>
2018-10-17 10:00:28 +0100

    Angular Routing Basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,8 +16,13 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 import { DishService } from './services/dish.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
 
 // Add an icon to the library for convenient access in other components
 library.add(fas, far, fab);
@@ -28,7 +33,10 @@ library.add(fas, far, fab);
     MenuComponent,
     DishdetailComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -36,7 +44,8 @@ library.add(fas, far, fab);
     MatToolbarModule,
     FlexLayoutModule,
     MaterialModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AppRoutingModule
   ],
   providers: [
     DishService
