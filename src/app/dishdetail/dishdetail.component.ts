9173d2a7843209a6f85ae9aef5228ce035f3c98b
jason-ibisi <daveyjones@grr.la>
2018-10-27 23:48:23 +0100

    Saving Changes to Server

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input, ViewChild, Inject } from '@angular/core';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { Dish } from '../shared/dish';
+import { Comment } from '../shared/comment';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
@@ -14,9 +15,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  tmpDish: Dish;
   dishIds: string[];
   prev: string;
   next: string;
+  comment: Comment;
   commentForm: FormGroup;
   commentStatus: boolean;
   errorMessage: string;
@@ -54,7 +57,7 @@ export class DishdetailComponent implements OnInit {
       .subscribe((dishIds) => this.dishIds = dishIds);
 
     this.route.params.pipe(switchMap((params: Params) => this.dishService.getDish(params['id'])))
-      .subscribe((dish) => { this.dish = dish; this.setPrevNext(dish.id); },
+      .subscribe((dish) => { this.dish = dish; this.tmpDish = dish; this.setPrevNext(dish.id); },
         errmessage => this.errorMessage = <any>errmessage);
   }
 
@@ -118,13 +121,15 @@ export class DishdetailComponent implements OnInit {
     this.commentStatus = false;
 
     // push comment to dish comments array in the dish model
-    const commentDate = new Date();
-    this.dish.comments.push({
-      author: this.commentForm.get('author').value,
-      comment: this.commentForm.get('comment').value,
-      rating: parseInt(this.commentForm.get('rating').value, 10),
-      date: commentDate.toISOString()
-    });
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISOString();
+    this.tmpDish.comments.push(this.comment);
+
+    this.dishService.putDish(this.tmpDish)
+      .subscribe(dish => {
+        this.dish = dish; this.tmpDish = dish;
+      },
+      errmessage => { this.dish = null; this.tmpDish = null; this.errorMessage = <any>errmessage; });
 
     // reset comment form values and pristine state
     this.commentFormDirective.resetForm();
